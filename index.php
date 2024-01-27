7f0f4470de3fd093b5bba8a37adf9ec43b04667e
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-01-27 16:54:11 -0600

    If url is null, fill in the fields

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
+
+$controller= "Pages";
+$accion= "inicio";
+
 if( isset($_GET['controller']) && isset($_GET['accion'])){
-    $controller= $_GET['controller'];
-    $accion= $_GET['accion'];
+
+    if(($_GET['controller']!="") && ($_GET['accion']!="")){  
+        $controller= $_GET['controller'];
+        $accion= $_GET['accion'];
+    }
+    
 }
 
 require("../Sistema/View/template.php");
--- router.php
+++ router.php
@@ -1,6 +1,4 @@
 <?php
-echo $controller;
-echo $accion;
 
 //Se manda a llamar "Controller/Pages_Controller.php" de manera dinámica en la siguiente línea
 include_once("Controller/".$controller."_Controller.php");

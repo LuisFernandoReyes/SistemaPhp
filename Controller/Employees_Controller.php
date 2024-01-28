4bde32a7fa978014a6f78a1afa188ba67b8916d0
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-01-27 18:37:28 -0600

    Making Create form and index form from Employees

--- Controller/Employees_Controller.php
+++ Controller/Employees_Controller.php
@@ -2,15 +2,15 @@
 class EmployeesController{
 
     public  function index() {
-
+        include_once("./View/Employees/Index.php");
     }
 
     public  function create() {
-
+        include_once("./View/Employees/Create.php");
     }
 
     public  function edit() {
-
+        include_once("./View/Employees/Edit.php");
     }
 
 
--- View/Employees/Index.php
+++ View/Employees/Index.php
@@ -1,1 +1,22 @@
-Index table
+<!--
+http://localhost/Sistema/?controller=Employees&accion=Index
+-->
+
+    <table class="table table-bordered" style="text-align: center;">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>E-Mail</th>
+                <th>Accions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Luis Fernando Reyes</td>
+                <td>lui5-fernando@hotmail.com</td>
+                <td>Edit | Delete</td>
+            </tr>
+        </tbody>
+    </table>

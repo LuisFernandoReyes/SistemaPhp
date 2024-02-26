52318b1f1cb077629fc444961766ed9a915fa93e
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-02-25 18:38:52 -0600

    Adding buttons and reading registers in the Index's employees

--- Controller/Employees_Controller.php
+++ Controller/Employees_Controller.php
@@ -5,7 +5,7 @@ DataBase::createInstance();
 class EmployeesController{
 
     public  function index() {
-        print_r(Employee::read());
+        $employees= Employee::read();
         include_once("./View/Employees/Index.php");
     }
 
--- View/Employees/Index.php
+++ View/Employees/Index.php
@@ -2,21 +2,29 @@
 http://localhost/Sistema/?controller=Employees&accion=Index
 -->
 
-    <table class="table table-bordered" style="text-align: center;">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>E-Mail</th>
-                <th>Accions</th>
+<table class="table table-bordered" style="text-align: center;">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>E-Mail</th>
+            <th>Accions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($employees as $employee) { ?>
+            <td> <?php echo $employee->id ?> </td>
+            <td> <?php echo $employee->name ?> </td>
+            <td> <?php echo $employee->email ?> </td>
+            <td>
+                <div class="btn-group" role="group" aria-label="Button group name">
+                    <a href="#" class="btn btn-info">Edit</a>
+                    <a href="#" class="btn btn-danger">Delete</a>
+
+                </div>
+            </td>
             </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Luis Fernando Reyes</td>
-                <td>lui5-fernando@hotmail.com</td>
-                <td>Edit | Delete</td>
-            </tr>
-        </tbody>
-    </table>
+
+        <?php } ?>
+    </tbody>
+</table>
